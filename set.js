0d5f76f8e370d32449d4a1eb69dae57be6f6b47d
richgagamidush <agagamidush@gmail.com>
2025-01-18 16:44:35 +0300

    Create set.js

---
+++ set.js
@@ -0,0 +1,50 @@
+const fs = require('fs-extra');
+const path = require("path");
+const { Sequelize } = require('sequelize');
+
+// Load environment variables if the .env file exists
+if (fs.existsSync('set.env')) {
+    require('dotenv').config({ path: __dirname + '/set.env' });
+}
+
+const databasePath = path.join(__dirname, './database.db');
+const DATABASE_URL = process.env.DATABASE_URL === undefined ? databasePath : process.env.DATABASE_URL;
+
+module.exports = {
+    session: process.env.SESSION_ID || 'GAGA-MD-WA-BOT;;;=>',
+    PREFIXES: (process.env.PREFIX || '.').split(',').map(prefix => prefix.trim()).filter(Boolean),
+    OWNER_NAME: process.env.OWNER_NAME || "Willis-Xth",
+    OWNER_NUMBER: process.env.OWNER_NUMBER || "254112291443,254729269118",
+    AUTO_READ_STATUS: process.env.AUTO_VIEW_STATUS || "on",
+    AUTOREAD_MESSAGES: process.env.AUTO_READ_MESSAGES || "off",
+    CHATBOT: process.env.CHAT_BOT || "off",
+    AUTO_DOWNLOAD_STATUS: process.env.AUTO_SAVE_STATUS || 'off',
+    A_REACT: process.env.AUTO_REACTION || 'off',
+    L_S: process.env.STATUS_LIKE || 'on',
+    AUTO_BLOCK: process.env.BLOCK_ALL || 'off',
+    URL: process.env.MENU_LINKS || 'https://telegra.ph/file/d7b133573a5a3622775e6.jpg',
+    MODE: process.env.BOT_MODE || "public",
+    PM_PERMIT: process.env.PM_PERMIT || 'on',
+    HEROKU_APP_NAME: process.env.HEROKU_APP_NAME,
+    HEROKU_API_KEY: process.env.HEROKU_API_KEY,
+    WARN_COUNT: process.env.WARN_COUNT || '3',
+    PRESENCE: process.env.PRESENCE || '',
+    ADM: process.env.ANTI_DELETE || 'on',
+    TZ: process.env.TIME_ZONE || 'Africa/Nairobi',
+    DP: process.env.STARTING_MESSAGE || "on",
+    ANTICALL: process.env.ANTICALL || 'off',
+    DATABASE_URL,
+    DATABASE: DATABASE_URL === databasePath
+        ? "postgresql://flashmd_user:JlUe2Vs0UuBGh0sXz7rxONTeXSOra9XP@dpg-cqbd04tumphs73d2706g-a/flashmd"
+        : "postgresql://flashmd_user:JlUe2Vs0UuBGh0sXz7rxONTeXSOra9XP@dpg-cqbd04tumphs73d2706g-a/flashmd",
+    W_M: null, // Add this line
+};
+
+// Watch for changes in this file and reload it automatically
+const fichier = require.resolve(__filename);
+fs.watchFile(fichier, () => {
+    fs.unwatchFile(fichier);
+    console.log(`Updated ${__filename}`);
+    delete require.cache[fichier];
+    require(fichier);
+});
