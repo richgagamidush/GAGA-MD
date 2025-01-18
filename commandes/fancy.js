23f73d066e6ce7222d1cb3fe67f902e051f1f977
richgagamidush <agagamidush@gmail.com>
2025-01-18 12:49:41 +0300

    Update fancy.js

--- commandes/fancy.js
+++ commandes/fancy.js
@@ -1,14 +1,14 @@
 const { zokou } = require("../framework/zokou");
 const fancy = require("../commandes/style");
 
-zokou({ nomCom: "fancy", categorie: "Fun", reaction: "〽️" }, async (dest, zk, commandeOptions) => {
+zokou({ nomCom: "fancy", categorie: "Fun", reaction: "💢" }, async (dest, zk, commandeOptions) => {
     const { arg, repondre, prefixe } = commandeOptions;
     const id = arg[0]?.match(/\d+/)?.join('');
     const text = arg.slice(1).join(" ");
 
     try {
         if (id === undefined || text === undefined) {
-            return await repondre(`\nExemple : ${prefixe}fancy 10 Anyway-Md\n` + String.fromCharCode(8206).repeat(4001) + fancy.list('ANYWAY-MD', fancy));
+            return await repondre(`\nExemple : ${prefixe}fancy 10 Gaga-MD\n` + String.fromCharCode(8206).repeat(4001) + fancy.list('GAGA-MD', fancy));
         }
 
         const selectedStyle = fancy[parseInt(id) - 1];
