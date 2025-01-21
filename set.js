d84af818bbe3524d98ddd9f47317034eec3e9358
richgagamidush <agagamidush@gmail.com>
2025-01-22 00:15:49 +0300

    Update set.js

--- set.js
+++ set.js
@@ -1,50 +1,52 @@
 const fs = require('fs-extra');
-const path = require("path");
 const { Sequelize } = require('sequelize');
-
-// Load environment variables if the .env file exists
-if (fs.existsSync('set.env')) {
+if (fs.existsSync('set.env'))
     require('dotenv').config({ path: __dirname + '/set.env' });
-}
-
+const path = require("path");
 const databasePath = path.join(__dirname, './database.db');
-const DATABASE_URL = process.env.DATABASE_URL === undefined ? databasePath : process.env.DATABASE_URL;
-
-module.exports = {
-    session: process.env.SESSION_ID || 'GAGA-MD-WA-BOT;;;=>',
-    PREFIXES: (process.env.PREFIX || '.').split(',').map(prefix => prefix.trim()).filter(Boolean),
-    OWNER_NAME: process.env.OWNER_NAME || "Willis-Xth",
-    OWNER_NUMBER: process.env.OWNER_NUMBER || "254112291443,254729269118",
-    AUTO_READ_STATUS: process.env.AUTO_VIEW_STATUS || "on",
-    AUTOREAD_MESSAGES: process.env.AUTO_READ_MESSAGES || "off",
-    CHATBOT: process.env.CHAT_BOT || "off",
-    AUTO_DOWNLOAD_STATUS: process.env.AUTO_SAVE_STATUS || 'off',
-    A_REACT: process.env.AUTO_REACTION || 'off',
-    L_S: process.env.STATUS_LIKE || 'on',
-    AUTO_BLOCK: process.env.BLOCK_ALL || 'off',
-    URL: process.env.MENU_LINKS || 'https://telegra.ph/file/d7b133573a5a3622775e6.jpg',
-    MODE: process.env.BOT_MODE || "public",
-    PM_PERMIT: process.env.PM_PERMIT || 'on',
-    HEROKU_APP_NAME: process.env.HEROKU_APP_NAME,
-    HEROKU_API_KEY: process.env.HEROKU_API_KEY,
-    WARN_COUNT: process.env.WARN_COUNT || '3',
-    PRESENCE: process.env.PRESENCE || '',
-    ADM: process.env.ANTI_DELETE || 'on',
-    TZ: process.env.TIME_ZONE || 'Africa/Nairobi',
-    DP: process.env.STARTING_MESSAGE || "on",
-    ANTICALL: process.env.ANTICALL || 'off',
+const DATABASE_URL = process.env.DATABASE_URL === undefined
+    ? databasePath
+    : process.env.DATABASE_URL;
+module.exports = { session: process.env.SESSION_ID || 'zokk',
+     ETAT:process.env.ETAT,
+    PREFIXE: process.env.PREFIXE,
+    NOM_OWNER: process.env.NOM_OWNER || "Zokou-Md",
+    NUMERO_OWNER : process.env.NUMERO_OWNER,              
+    LECTURE_AUTO_STATUS: process.env.LECTURE_AUTO_STATUS || "non",
+    TELECHARGER_AUTO_STATUS: process.env.TELECHARGER_AUTO_STATUS || 'non',
+    MODE: process.env.MODE_PUBLIC,
+    PM_PERMIT: process.env.PM_PERMIT || 'non',
+    BOT : process.env.NOM_BOT || 'Zokou_MD',
+    URL : process.env.LIENS_MENU || 'https://static.animecorner.me/2023/08/op2.jpg',
+    HEROKU_APP_NAME : process.env.HEROKU_APP_NAME,
+    HEROKU_APY_KEY : process.env.HEROKU_APY_KEY ,
+    WARN_COUNT : process.env.WARN_COUNT || '3' ,
+    //GPT : process.env.OPENAI_API_KEY,
+    DP : process.env.STARTING_BOT_MESSAGE || 'oui',
+    ATD : process.env.ANTI_DELETE_MESSAGE || 'non',            
     DATABASE_URL,
     DATABASE: DATABASE_URL === databasePath
-        ? "postgresql://flashmd_user:JlUe2Vs0UuBGh0sXz7rxONTeXSOra9XP@dpg-cqbd04tumphs73d2706g-a/flashmd"
-        : "postgresql://flashmd_user:JlUe2Vs0UuBGh0sXz7rxONTeXSOra9XP@dpg-cqbd04tumphs73d2706g-a/flashmd",
-    W_M: null, // Add this line
+        ? "postgres://db_7xp9_user:6hwmTN7rGPNsjlBEHyX49CXwrG7cDeYi@dpg-cj7ldu5jeehc73b2p7g0-a.oregon-postgres.render.com/db_7xp9" : "postgres://db_7xp9_user:6hwmTN7rGPNsjlBEHyX49CXwrG7cDeYi@dpg-cj7ldu5jeehc73b2p7g0-a.oregon-postgres.render.com/db_7xp9",
+    /* new Sequelize({
+     dialect: 'sqlite',
+     storage: DATABASE_URL,
+     logging: false,
+})
+: new Sequelize(DATABASE_URL, {
+     dialect: 'postgres',
+     ssl: true,
+     protocol: 'postgres',
+     dialectOptions: {
+         native: true,
+         ssl: { require: true, rejectUnauthorized: false },
+     },
+     logging: false,
+}),*/
 };
-
-// Watch for changes in this file and reload it automatically
-const fichier = require.resolve(__filename);
+let fichier = require.resolve(__filename);
 fs.watchFile(fichier, () => {
     fs.unwatchFile(fichier);
-    console.log(`Updated ${__filename}`);
+    console.log(`mise à jour ${__filename}`);
     delete require.cache[fichier];
     require(fichier);
 });
