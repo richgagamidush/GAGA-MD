0f90632238b0ef6264d23625bd096586f0a4ff49
richgagamidush <agagamidush@gmail.com>
2025-01-26 12:04:13 +0300

    Update Answer.js

--- commandes/Answer.js
+++ commandes/Answer.js
@@ -1,125 +1,33 @@
-const { zokou } = require('../framework/zokou');
-const traduire = require("../framework/traduction") ;
-const { default: axios } = require('axios');
-//const conf = require('../set');
-
-
-
-
-zokou({nomCom:"xgaga",reaction:"📡",categorie:"IA"},async(dest,zk,commandeOptions)=>{
-
-  const {repondre,ms,arg}=commandeOptions;
-  
-    if(!arg || !arg[0])
-    {return repondre("yes I'm listening to you.")}
-    //var quest = arg.join(' ');
-  try{
-    
-    
-const message = await traduire(arg.join(' '),{ to : 'en'});
- console.log(message)
-fetch(`https://api.dreaded.site/api/makgpt?text=[uid]&msg=${message}`)
-.then(response => response.json())
-.then(data => {
-  const botResponse = data.cnt;
-  console.log(botResponse);
-
-  traduire(botResponse, { to: 'en' })
-    .then(translatedResponse => {
-      repondre(translatedResponse);
-    })
-    .catch(error => {
-      console.error('Error when translating into French :', error);
-      repondre('Error when translating into French');
-    });
-})
-.catch(error => {
-  console.error('Error requesting BrainShop :', error);
-  repondre('Error requesting BrainShop');
+const {
+  zokou
+} = require("../framework/zokou");
+const {
+  default: axios
+} = require("axios");
+zokou({
+  'nomCom': "gemini",
+  'reaction': '📡',
+  'categorie': 'AI'
+}, async (_0x40b158, _0x2101a0, _0x2563b9) => {
+  const {
+    repondre: _0x49381c,
+    arg: _0x207296,
+    ms: _0x4dd555
+  } = _0x2563b9;
+  try {
+    if (!_0x207296 || _0x207296.length === 0) {
+      return _0x49381c("Please ask a question Gaga will answer it.");
+    }
+    const _0x5529b9 = _0x207296.join(" ");
+    const _0x303006 = await axios.get("https://api.giftedtech.us.kg/api/ai/geminiai?q=" + _0x5529b9 + "&apikey=" + "gifted");
+    const _0x20bc85 = _0x303006.data;
+    if (_0x20bc85) {
+      _0x49381c(_0x20bc85.result);
+    } else {
+      _0x49381c("Error during response generation.");
+    }
+  } catch (_0x4baeb0) {
+    console.error("Erreur:", _0x4baeb0.message || "Une erreur s'est produite");
+    _0x49381c("Oops, an error occurred while processing your request.");
+  }
 });
-
-  }catch(e){ repondre("oops an error : "+e)}
-    
-  
-  });  
-
-
-
-  zokou({ nomCom: "gemini", reaction: "📡", categorie: "IA" }, async (dest, zk, commandeOptions) => {
-    const { repondre, arg, ms } = commandeOptions;
-  
-    try {
-      if (!arg || arg.length === 0) {
-        return repondre(`Please enter the necessary information to generate the image.`);
-      }
-  
-      // Regrouper les arguments en une seule chaîne séparée par "-"
-      const image = arg.join(' ');
-      const response = await axios.get(`https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=GEMINI_API_KEY=${image}`);
-      
-      const data = response.data;
-      let caption = '*powered by BMW-MD*';
-      
-      if (data.status == 200) {
-        // Utiliser les données retournées par le service
-        const imageUrl = data.result;
-        zk.sendMessage(dest, { image: { url: imageUrl }, caption: caption }, { quoted: ms });
-      } else {
-        repondre("Error during image generation.");
-      }
-    } catch (error) {
-      console.error('Erreur:', error.message || 'Une erreur s\'est produite');
-      repondre("Oops, an error occurred while processing your request");
-    }
-  });
-  
-  zokou({ nomCom: "gaga", reaction: "🌚", categorie: "IA" }, async (dest, zk, commandeOptions) => {
-    const { repondre, arg, ms } = commandeOptions;
-  
-    try {
-      if (!arg || arg.length === 0) {
-        return repondre(`Please ask a me any thing.`);
-      }
-  
-      // Regrouper les arguments en une seule chaîne séparée par "-"
-      const question = arg.join(' ');
-      const response = await axios.get(`https://api.dreaded.site/api/makgpt?text=" + encodeURIComponent(_0x3c0d7c));
-      const data = response.data;
-      if (data) {
-        repondre(data.result);
-      } else {
-        repondre("Error during response generation.");
-      }
-    } catch (error) {
-      console.error('Erreur:', error.message || 'Une erreur s\'est produite');
-      repondre("Oops, an error occurred while processing your request.");
-    }
-  });
-
-
-zokou({ nomCom: "gpt", reaction: "🤔", categorie: "IA" }, async (dest, zk, commandeOptions) => {
-    const { repondre, arg, ms } = commandeOptions;
-  
-    try {
-      if (!arg || arg.length === 0) {
-        return repondre(`Please ask a question.`);
-      }
-  
-      // Regrouper les arguments en une seule chaîne séparée par "-"
-      const question = arg.join(' ');
-      const response = await axios.get(`https://gpt4.giftedtech.workers.dev/?prompt=${question}`);
-      
-      const data = response.data;
-      if (data) {
-        repondre(data.result);
-      } else {
-        repondre("Error during response generation.");
-      }
-    } catch (error) {
-      console.error('Erreur:', error.message || 'Une erreur s\'est produite');
-      repondre("Oops, an error occurred while processing your request.");
-    }
-  });
-
-
-  
