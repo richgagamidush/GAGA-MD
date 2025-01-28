86e4bf7e4f0f6dff0a76b324bb472c1998a93e8b
richgagamidush <agagamidush@gmail.com>
2025-01-28 11:10:56 +0300

    Update body.js

--- body.js
+++ body.js
@@ -94,9 +94,9 @@ setTimeout(() => {
                 /** caching makes the store faster to send/recv messages */
                 keys: (0, baileys_1.makeCacheableSignalKeyStore)(state.keys, logger),
             },
-            //////////
-            "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+            /////////
+            
+  var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
     if (k2 === undefined) k2 = k;
   var desc = Object.getOwnPropertyDescriptor(m, k);
     if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
