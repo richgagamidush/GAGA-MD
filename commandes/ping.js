3b0b973a4c971d8f66eb82c5bf60f8a8668ee426
richgagamidush <agagamidush@gmail.com>
2025-01-28 08:18:13 +0300

    Update ping.js

--- commandes/ping.js
+++ commandes/ping.js
@@ -1,22 +1,89 @@
-const { zokou } = require("../framework/zokou");
-const moment = require("moment-timezone");
-const { default: axios } = require('axios');
-//const conf = require('../set');
+//const _0x4b5ec8=_0x2698;function _0x2698(_0x260844,_0x49398f){const _0xf45f7b=_0x7ad1();return _0x2698=function(_0x4f132,_0x3bdc33){_0x4f132=_0x4f132-(-0x51e+0x1c5c+-0x1f3*0xb);let _0x1775e2=_0xf45f7b[_0x4f132];return _0x1775e2;},_0x2698(_0x260844,_0x49398f);}function _0x7ad1(){const _0x152234=['getTime','9414823OsthPz','500CCFQEU','1200tUlniz','*\x20𝗠𝘀\x20','SPEED\x20TEST','BELTAH-MD\x20','e-now','𝗦𝗣𝗘𝗘𝗗\x20::::','work/zokou','ping','6NPonHO','General','../set','12iYIfGT','🙂‍↕️','key','sendMessag','./../frame','tions','../framewo','KKMNE','34937tkVJLE','jsqwE','1749212DiqyeJ','𝗕𝗘𝗟𝗧𝗔𝗛-𝗠𝗗\x20','VoaVq','157655EJhvPy','performanc','rk/mesfonc','34054sKGZBG','......','393446oUPSlE','55008HrIbzV','perf_hooks'];_0x7ad1=function(){return _0x152234;};return _0x7ad1();}(function(_0x35532d,_0x19ab1e){const _0x1b3cfd=_0x2698,_0x3ae991=_0x35532d();while(!![]){try{const _0x4f14c3=parseInt(_0x1b3cfd(0x1ed))/(-0x19c7*-0x1+-0x163a+-0x4*0xe3)+parseInt(_0x1b3cfd(0x1eb))/(-0x1fb9+0xc9a+0x1321)*(parseInt(_0x1b3cfd(0x1d8))/(-0xf7a+0x2095+-0x1118*0x1))+parseInt(_0x1b3cfd(0x1e5))/(0x1*-0x599+-0x1*-0x10df+-0xb42)+-parseInt(_0x1b3cfd(0x1e8))/(-0x115*0x1f+-0xc79+-0x1*-0x2e09)*(parseInt(_0x1b3cfd(0x1db))/(0x1*-0x13b4+0x17*-0x113+-0x145*-0x23))+parseInt(_0x1b3cfd(0x1e3))/(-0x6*-0x3d7+0x2*-0xba7+-0x5*-0xf)*(parseInt(_0x1b3cfd(0x1d0))/(-0xa0d+0x2e4*-0x1+-0x171*-0x9))+parseInt(_0x1b3cfd(0x1ee))/(-0x141+0x1003+-0xeb9*0x1)*(-parseInt(_0x1b3cfd(0x1cf))/(0x243c*0x1+-0x22b9+-0xd*0x1d))+-parseInt(_0x1b3cfd(0x1ce))/(-0x1890+-0x20f*-0xb+0x2*0xfb);if(_0x4f14c3===_0x19ab1e)break;else _0x3ae991['push'](_0x3ae991['shift']());}catch(_0x4b5e67){_0x3ae991['push'](_0x3ae991['shift']());}}}(_0x7ad1,0x3*-0x4a0c+-0xa3e9a*-0x1+-0x37154));const {zokou}=require(_0x4b5ec8(0x1df)+_0x4b5ec8(0x1d6)),{format,runtime}=require(_0x4b5ec8(0x1e1)+_0x4b5ec8(0x1ea)+_0x4b5ec8(0x1e0)),os=require('os'),speed=require(_0x4b5ec8(0x1e9)+_0x4b5ec8(0x1d4)),{performance}=require(_0x4b5ec8(0x1ef)),conf=require(_0x4b5ec8(0x1da));zokou({'nomCom':_0x4b5ec8(0x1d7),'categorie':_0x4b5ec8(0x1d9),'reaction':'🏮','alias':['p']},async(_0xd84f4a,_0x39990a,_0x5171f4)=>{const _0x5efab0=_0x4b5ec8,_0x20880e={'jsqwE':_0x5efab0(0x1d3)+_0x5efab0(0x1d2)+_0x5efab0(0x1ec),'KKMNE':function(_0x591db3,_0x205874){return _0x591db3-_0x205874;},'VoaVq':_0x5efab0(0x1dc)},{ms:_0x5e8cfd,arg:_0x19e67c,repondre:_0x13a5f9}=_0x5171f4,_0x468a2a=new Date()[_0x5efab0(0x1cd)](),_0x2fa865=await _0x39990a[_0x5efab0(0x1de)+'e'](_0xd84f4a,{'text':_0x20880e[_0x5efab0(0x1e4)]},{'quoted':_0x5e8cfd}),_0x275495=new Date()[_0x5efab0(0x1cd)](),_0x33b670=_0x20880e[_0x5efab0(0x1e2)](_0x275495,_0x468a2a);await _0x39990a[_0x5efab0(0x1de)+'e'](_0xd84f4a,{'text':_0x5efab0(0x1e6)+_0x5efab0(0x1d5)+'\x20*'+_0x33b670+_0x5efab0(0x1d1),'edit':{'id':_0x2fa865[_0x5efab0(0x1dd)]['id'],'remoteJid':_0xd84f4a}}),await _0x39990a[_0x5efab0(0x1de)+'e'](_0xd84f4a,{'react':{'text':_0x20880e[_0x5efab0(0x1e7)],'key':_0x5e8cfd[_0x5efab0(0x1dd)]}});});
 
-
-zokou({ nomCom: 'ping',
-    desc: 'To check ping',
-    Categorie: 'General',
-    reaction: '🚀', 
-    fromMe: 'true', 
-
-       
-  },
-  async (dest, zk, commandeOptions) => {
-    const { ms, arg, repondre } = commandeOptions;
-    const { start} = new Date().getTime()
-    return repondre('https://whatsapp.com/channel/0029VasnifMFi8xW4Mqysn2F') 
-    const { end } = new Date().getTime()
-    await zok.sendMessage('*𝗣𝗼𝗻𝗴!*\n ```' + (end - start) + '``` *ms*')
-  }
-)
+const {
+  zokou
+} = require("../framework/zokou");
+function delay(_0x25f170) {
+  console.log("â±ï¸ delay for " + _0x25f170 + 'ms');
+  return new Promise(_0x529a1f => setTimeout(_0x529a1f, _0x25f170));
+}
+zokou({
+  'nomCom': "ping",
+  'desc': "To check bot response time",
+  'Categorie': "General",
+  'reaction': '✈️',
+  'fromMe': "true"
+}, async (_0x4cfdc6, _0x41d5d3, _0x546e3d) => {
+  const {
+    ms: _0x2c968b
+  } = _0x546e3d;
+  const _0x5a815c = Array.from({
+    'length': 0x3
+  }, () => Math.floor(Math.random() * 10000 + 1000));
+  const _0x451c3b = _0x5a815c.map(_0x526b71 => "🔴" + _0x526b71 + "🔴").join("\n");
+  const _0x41d2b1 = {
+    'externalAdReply': {
+      'title': "GAGA MD",
+      'body': "ᴘɪɴɢ : " + _0x451c3b,
+      'sourceUrl': "https://whatsapp.com/channel/0029VasnifMFi8xW4Mqysn2F",
+      'thumbnailUrl': "https://files.catbox.moe/0zqr00.jpg",
+      'mediaType': 0x1,
+      'showAdAttribution': true
+    }
+  };
+  const _0x54ea81 = {
+    'displayName': "GAGA MD",
+    'vcard': "BEGIN:VCARD\nVERSION:3.0\nFN:G \nORG:BELTAH Nexus;\nTEL;type=CELL;type=VOICE;waid=254114141192:254737681758 \nEND:VCARD"
+  };
+  await _0x41d5d3.sendMessage(_0x4cfdc6, {
+    'text': "GAGA MD speed",
+    'contextInfo': _0x41d2b1,
+    'contacts': {
+      'displayName': "GAGA MD Verified Contact",
+      'contacts': [_0x54ea81]
+    },
+    'quoted': _0x2c968b
+  });
+  console.log("Ping results sent successfully with contact and context info!");
+});
+zokou({
+  'nomCom': "uptime",
+  'desc': "To check runtime",
+  'Categorie': "General",
+  'reaction': '🚀',
+  'fromMe': "true"
+}, async (_0x205206, _0x161605, _0x2fdaee) => {
+  const {
+    ms: _0x4e9ae0,
+    repondre: _0x2a6ebf
+  } = _0x2fdaee;
+  const _0x766aaf = process.uptime();
+  const _0x2f3abc = new Date(_0x766aaf * 1000).toISOString().substr(11, 8);
+  const _0xae590 = {
+    'externalAdReply': {
+      'title': "GAGA SETTINGS",
+      'body': "👻 ʙᴇʟᴛᴀʜ ᴛᴇᴄʜ ʙᴏᴛ ɴᴇᴠᴇʀ sʟᴇᴇᴘs 👻",
+      'sourceUrl': "https://whatsapp.com/channel/0029VasnifMFi8xW4Mqysn2F",
+      'thumbnailUrl': "https://files.catbox.moe/0zqr00.jpg",
+      'mediaType': 0x1,
+      'showAdAttribution': true
+    }
+  };
+  const _0x297b69 = {
+    'displayName': "XGAGA UPDATES",
+    'vcard': "BEGIN:VCARD\nVERSION:3.0\nFN:BELTAH XBOT\nORG: BELTAH XBOT Nexus;\nTEL;type=CELL;type=VOICE;waid=254114141192 :254737681758\nEND:VCARD"
+  };
+  await _0x161605.sendMessage(_0x205206, {
+    'text': "Gaga md uptime :" + _0x2f3abc,
+    'contextInfo': _0xae590,
+    'contacts': {
+      'displayName': "GAGA TECH Verified Contact",
+      'contacts': [_0x297b69]
+    },
+    'quoted': _0x4e9ae0
+  });
+  console.log("Uptime sent successfully with contact and context info!");
+});
+module.exports = {
+  'delay': delay
+}; 
